bdf63a372ca19efae78cd593f841b5c12138edf2
Leo <kanisornchuti@gmail.com>
2026-09-13 23:24:12 +0700

    feat: new feature in fixedtest-git

--- fixc/fixedtest-git.c
+++ fixc/fixedtest-git.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
 int main()
 {
+    int num1, num2;
     printf("I love CE!!!");
+    printf("\nEnter number: ");
+    scanf("%d", &num1);
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+    num1 += num2;
+    printf("The sum of two numbers is: %d", num1);
     return 0;
 }
